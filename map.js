c384058c8075dd3a639f3dce51c69a95afcd48e9
Olli Etuaho <olli.etuaho@gmail.com>
2015-04-18 13:09:40 +0300

    More stuff on map

--- map.js
+++ map.js
@@ -16,9 +16,25 @@ var mapData = [
     '     LLL     '
 ];
 
+var MapTile = function(typeStr) {
+    this.typeStr = typeStr;
+};
+
+MapTile.prototype.render = function() {
+    
+};
+
 /**
  * @constructor
  */
 var Map = function() {
-    
+    this.tiles = [];
+    for (var i = 0; i < mapData.length; ++i) {
+        var row = mapData[i];
+        var tilesRow = [];
+        for (var j = 0; j < row.length; ++j) {
+            
+        }
+        this.tiles.push(tilesRow);
+    }
 };
