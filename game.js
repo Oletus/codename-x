db10243dea7971763a22e0454f97133595d154ba
Olli Etuaho <olli.etuaho@gmail.com>
2015-04-18 10:48:49 +0300

    Separate game.js from game.html

---
+++ game.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var Game = function(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d');
+    this.time = 0;
+};
+
+Game.prototype.render = function() {
+    this.ctx.fillStyle = cssUtil.rgbString([0, 0, (Math.sin(this.time) * 0.5 + 0.5) * 255]);
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    return this.ctx;
+};
+
+Game.prototype.update = function(deltaTime) {
+    this.time += deltaTime;
+};
