248f0343558f61b2ddbca92cbc182528eec656fb
Olli Etuaho <olli.etuaho@gmail.com>
2015-04-18 12:08:04 +0300

    Add initial hex map data

--- map.js
+++ map.js
@@ -1,1 +1,24 @@
 "use strict";
+
+var mapData = [
+    '      MLLL  L',
+    '   M  MLL LLL',
+    '    L  LL  LL',
+    '  L L  L  LLL',
+    '  L L LLLLLLL',
+    '     LLLLLLLL',
+    '   LLLLLLLLLL',
+    '   LLLMMMLLLL',
+    '    LLLL LLL ',
+    '  LML  L LLL ',
+    '  LLL   L L L',
+    '  LL        L',
+    '     LLL     '
+];
+
+/**
+ * @constructor
+ */
+var Map = function() {
+    
+};
