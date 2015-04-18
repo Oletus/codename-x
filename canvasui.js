3548d0eb2924bc2c927bbab9c39f2885d4a70a7c
Olli Etuaho <olli.etuaho@gmail.com>
2015-04-18 15:41:05 +0300

    Add UI buttons

---
+++ canvasui.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var CanvasButton = function(options) {
+    var defaults = {
+        label: 'Button',
+        centerX: 0,
+        centerY: 0,
+        width: 100,
+        height: 50
+    };
+    for(var key in defaults) {
+        if (!options.hasOwnProperty(key)) {
+            this[key] = defaults[key];
+        } else {
+            this[key] = options[key];
+        }
+    }
+};
+
+CanvasButton.prototype.render = function(ctx, cursorX, cursorY) {
+    var rect = this.getRect();
+    ctx.fillStyle = '#000';
+    if (this.hitTest(cursorX, cursorY))
+    {
+        ctx.globalAlpha = 1.0;
+    } else {
+        ctx.globalAlpha = 0.5;
+    }
+    ctx.fillRect(rect.left, rect.top, rect.width(), rect.height());
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#fff';
+    ctx.strokeRect(rect.left, rect.top, rect.width(), rect.height());
+    ctx.globalAlpha = 1.0;
+    ctx.textAlign = 'center';
+    ctx.fillStyle = '#fff';
+    ctx.font = '20px sans-serif';
+    ctx.fillText(this.label, this.centerX, this.centerY + 7);
+};
+
+CanvasButton.prototype.hitTest = function(x, y) {
+    return this.getRect().mightIntersectCircleRoundedOut(x, y, 1);
+};
+
+CanvasButton.prototype.getRect = function() {
+    return new Rect(
+        this.centerX - this.width * 0.5,
+        this.centerX + this.width * 0.5,
+        this.centerY - this.height * 0.5,
+        this.centerY + this.height * 0.5
+    );
+};
